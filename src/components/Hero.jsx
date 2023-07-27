f59359ebbfb50228120bcdb2448d203e6b7eecff
Jolene Kearse <pharaohnutz@gmail.com>
2023-07-27 15:48:09 -0400

    chore: update hero text className

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='hero container'>
       <img src={hero} className="hero-img" alt="people enjoying their airbnb experiences" />
       <h1 className="hero-title">Online Experiences</h1>
-      <p className="hero-intro">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
     </div>
   )
 }
