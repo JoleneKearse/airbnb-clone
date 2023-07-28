87ca7d65e20a7d00cc704353781506283c8816fd
Jolene Kearse <pharaohnutz@gmail.com>
2023-07-28 14:58:42 -0400

    feat: convert card from hard-coded values

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import experiencesData from "./data"
 
 function App() {
+  const cards = experiencesData.map(experience => {
+    return (
+      <Card
+        key={experience.key}
+        coverImg={experience.coverImg}
+        rating={experience.stats.rating}
+        reviewCount={experience.stats.reviewCount}
+        location={experience.location}
+        title={experience.title}
+        price={experience.price}
+        openSpots={experience.openSpots}
+      />
+    )
+  })
 
   return (
     <>
       <NavBar />
       <Hero />
-      <Card 
-        img="katie-zaferes.png"
-        label="sold out"
-        rating="5.0"
-        ratingCount={6}
-        country="USA"
-        blurb="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className="cards-list">
+        {cards}
+      </section>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/prop-types */
 import star from "../assets/star.png"
 
-const Card = ({ img, label, rating, ratingCount, country, blurb, price }) => {
+const Card = ({ coverImg, rating, reviewCount, location, title, price, openSpots }) => {
+  let labelText
+  if (!openSpots) {
+    labelText = "sold out"
+  } else if (location === "Online") {
+    labelText = "online"
+  }
+
   return (
-    <section className="container">
+    <section className="card">
       <div className="section-img">
-        <img src={`../public/${img}`} className="section-img-thumbnail" alt="" />
-        <p className="section-img-label">{label}</p>
+        <img src={`../public/${coverImg}`} className="section-img-thumbnail" alt="" />
+        {labelText && <p className="section-img-label">{labelText}</p>}
       </div>
       <div className="section-info">
         <div className="section-info-details">
           <img src={star} alt="" />
           <p className="section-info-details-rating">{rating}</p>
-          <p className="section-info-details-customers">({ratingCount})</p>
-          <p className="section-info-details-country"><span className="section-info-details-dot">&#x2022; </span>{country}</p>
+          <p className="section-info-details-customers">({reviewCount})</p>
+          <p className="section-info-details-country"><span className="section-info-details-dot">&#x2022; </span>{location}</p>
         </div>
-        <p className="section-info-blurb">{blurb}</p>
+        <p className="section-info-blurb">{title}</p>
         <p className="section-info rate"><span className="section-info-pricing">From ${price}</span> / person</p>
       </div>
     </section>
